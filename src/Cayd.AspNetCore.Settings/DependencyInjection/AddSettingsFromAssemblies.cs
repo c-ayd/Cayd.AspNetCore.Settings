89b044eb6548bf2d35edad2e4a7e1ac32532f1fb
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-05-25 14:19:35 +0200

    add di method registering settings from more than one assembly

---
+++ src/Cayd.AspNetCore.Settings/DependencyInjection/AddSettingsFromAssemblies.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Reflection;
+
+namespace Cayd.AspNetCore.Settings.DependencyInjection
+{
+    public static partial class DependencyInjectionExtensions
+    {
+        public static void AddSettingsFromAssemblies(this IHostApplicationBuilder builder, params Assembly[] assemblies)
+        {
+            foreach (var assembly in assemblies)
+            {
+                AddSettings(builder, assembly);
+            }
+        }
+    }
+}
